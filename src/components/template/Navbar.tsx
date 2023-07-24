5b25de5e2c5862107123b78a681dcafc90e38c7f
shadi-giorgio-islam <shadigiorgioislam@gmail.com>
2023-07-24 17:54:38 +0200

    add NavBar responsive and footer responsive

--- src/components/template/Navbar.tsx
+++ src/components/template/Navbar.tsx
@@ -1,54 +1,123 @@
-import {AppBar, Toolbar, IconButton, Button, Menu, MenuItem, Stack, Typography} from "@mui/material";
-import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
-import MenuBookRoundedIcon from '@mui/icons-material/MenuBookRounded';
-import React, {useState} from "react";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
 
+const pages = ["Home", "Gruppi", "Tools", "Blog", "About"];
 export const Navbar = () => {
-    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
-    const open = Boolean(anchorEl)
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        setAnchorEl(event.currentTarget)
-    }
-    const handleClose = () => {
-      setAnchorEl(null)
-    }
-  return (
-        <AppBar position='static'>
-            <Toolbar>
-                <IconButton size='large' edge='start' color='inherit' aria-label='logo'>
-                    <MenuBookRoundedIcon/>
-                </IconButton>
-                <Typography variant='h6' component='div' sx={{flexGrow: 1}}>SOSUNI</Typography>
-                <Stack direction='row' spacing={2}>
-                    <Button color='inherit'>Gruppi esame</Button>
-                    <Button color='inherit'
-                            id='resources-button'
-                            onClick={handleClick}
-                            aria-controls={open ? 'resources-menu' : undefined}
-                            aria-haspopup='true'
-                            aria-expanded= {open ? 'true' : undefined}
-                            endIcon={<KeyboardArrowDown/>}
-                    >Tools</Button>
-                    <Button color='inherit'>Blog</Button>
-                    <Button color='inherit'>About</Button>
-                </Stack>
-                <Menu id='resources-menu'
-                      anchorEl={anchorEl}
-                      open={open}
-                      MenuListProps={{'aria-labelledby': 'resources-button'}}
-                      onClose={handleClose}
-                      anchorOrigin={{
-                          vertical : 'bottom',
-                          horizontal : 'right'
-                      }}
-                      transformOrigin={{
-                          vertical : 'top',
-                          horizontal : 'right'
-                      }}
-                >
-                    <MenuItem>Sbobinator</MenuItem>
-                </Menu>
-            </Toolbar>
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+        null
+    );
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: "none", md: "flex" },
+                            fontFamily: "monospace",
+                            fontWeight: 700,
+                            letterSpacing: ".3rem",
+                            color: "inherit",
+                            textDecoration: "none",
+                        }}
+                    >
+                        LOGO
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: "bottom",
+                                horizontal: "left",
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: "top",
+                                horizontal: "left",
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: "block", md: "none" },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">{page}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href=""
+                        sx={{
+                            mr: 2,
+                            display: { xs: "flex", md: "none" },
+                            flexGrow: 1,
+                            fontFamily: "monospace",
+                            fontWeight: 700,
+                            letterSpacing: ".3rem",
+                            color: "inherit",
+                            textDecoration: "none",
+                        }}
+                    >
+                        LOGO
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, color: "white", display: "block" }}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
+
+                </Toolbar>
+            </Container>
         </AppBar>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import {Navbar} from './components/template/Navbar';
 import reportWebVitals from './reportWebVitals';
+import {Footer} from "./components/template/Footer";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,6 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Navbar />
+    <Footer/>
   </React.StrictMode>
 );
 
