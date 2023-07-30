0ce78074338aaada2ac5ff43b7b5aebd40023eff
shadi <shadigiorgioislam@gmail.com>
2023-07-31 00:29:45 +0200

    Add style footer

--- src/components/template/Footer.tsx
+++ src/components/template/Footer.tsx
@@ -5,67 +5,58 @@ import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import { Facebook, Instagram, Twitter } from "@mui/icons-material";
 import { Box } from "@mui/material";
+import './Footer.css';
 
 export const Footer = () => {
     return (
-        <Box
-            component="footer"
-            sx={{
-                backgroundColor: (theme) =>
-                    theme.palette.mode === "light"
-                        ? theme.palette.grey[200]
-                        : theme.palette.grey[800],
-                p: 6,
-            }}
-        >
+        <Box component="footer" className="box">
             <Container maxWidth="lg">
                 <Grid container spacing={5}>
                     <Grid item xs={12} sm={4}>
-                        <Typography variant="h6" color="text.primary" gutterBottom>
+                        <Typography variant="h6" gutterBottom>
                             About Us
                         </Typography>
-                        <Typography variant="body2" color="text.secondary">
+                        <Typography variant="body2">
                             We are XYZ company, dedicated to providing the best service to our
                             customers.
                         </Typography>
                     </Grid>
                     <Grid item xs={12} sm={4}>
-                        <Typography variant="h6" color="text.primary" gutterBottom>
+                        <Typography variant="h6" gutterBottom>
                             Contact Us
                         </Typography>
-                        <Typography variant="body2" color="text.secondary">
+                        <Typography variant="body2">
                             123 Main Street, Anytown, USA
                         </Typography>
-                        <Typography variant="body2" color="text.secondary">
+                        <Typography variant="body2">
                             Email: info@example.com
                         </Typography>
-                        <Typography variant="body2" color="text.secondary">
+                        <Typography variant="body2">
                             Phone: +1 234 567 8901
                         </Typography>
                     </Grid>
                     <Grid item xs={12} sm={4}>
-                        <Typography variant="h6" color="text.primary" gutterBottom>
+                        <Typography variant="h6" gutterBottom>
                             Follow Us
                         </Typography>
-                        <Link href="https://www.facebook.com/" color="inherit">
+                        <Link href="https://www.facebook.com/">
                             <Facebook />
                         </Link>
                         <Link
                             href="https://www.instagram.com/"
-                            color="inherit"
                             sx={{ pl: 1, pr: 1 }}
                         >
                             <Instagram />
                         </Link>
-                        <Link href="https://www.twitter.com/" color="inherit">
+                        <Link href="https://www.twitter.com/">
                             <Twitter />
                         </Link>
                     </Grid>
                 </Grid>
                 <Box mt={5}>
-                    <Typography variant="body2" color="text.secondary" align="center">
+                    <Typography variant="body2" align="center">
                         {"Copyright © "}
-                        <Link color="inherit" href="https://your-website.com/">
+                        <Link href="https://your-website.com/">
                             Your Website
                         </Link>{" "}
                         {new Date().getFullYear()}
