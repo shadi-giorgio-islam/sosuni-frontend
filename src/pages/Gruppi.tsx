0db9138d692bb3c443cd30d6fe3fc6832937864b
shadi <shadigiorgioislam@gmail.com>
2023-08-07 23:56:44 +0200

    Add appSearch

--- src/pages/Gruppi.tsx
+++ src/pages/Gruppi.tsx
@@ -1,10 +1,12 @@
 import Template from "../components/template/Template";
 import React from "react";
+import SearchBar from "../components/searchBar/SearchBar";
 
 export const Gruppi = () => {
     return (
         <Template>
             <h1>Gruppi</h1>
+            <SearchBar/>
         </Template>
     );
 }
