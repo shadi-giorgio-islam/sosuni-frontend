5d779d3c9bffb5ded4ab7514fb75ea114f5ec71b
shadi <shadigiorgioislam@gmail.com>
2023-08-22 15:17:09 +0200

    Add searchBar

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,10 +1,12 @@
 import Template from "../components/template/Template";
 import React from "react";
 import {BlogPagination} from "../components/blogPagination/BlogPagination";
+import SearchBar from "../components/searchBar/SearchBar";
 
 export const Blog = () => {
     return (
         <Template>
+            <SearchBar/>
             <BlogPagination/>
         </Template>
     );
