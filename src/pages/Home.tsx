dc4bd27f37e0215b33a4dfc85761af1b77dc774a
shadi <shadigiorgioislam@gmail.com>
2023-08-27 16:09:01 +0200

    Aggiunto sezione articoli sulla home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,25 @@ import Template from "../components/template/Template";
 import React from "react";
 import Banner from "../components/banner/Banner";
 import GroupSection from "../components/groupSection/GroupSection";
+import ArticleList from "../components/article/ArticleList";
+import Article from "../components/article/entity/Article";
+import {generateRandomArticles} from "../components/article/function/GenerateArticles";
+import "./Home.css";
+import {Typography} from "@mui/material";
 
 export const Home = () => {
+    const articles: Article[] = generateRandomArticles(50);
+    const displayedArticles = articles.slice(45, 50).reverse();
     return (
         <Template>
             <Banner/>
             <GroupSection/>
+            <div className="articoli-home">
+                <Typography variant="h5" className="article-home-text">
+                    Tieniti sempre informato! Leggi le ultime notizie sul mondo universitario:
+                </Typography>
+                <ArticleList articles={displayedArticles}/>
+            </div>
         </Template>
     );
 }
